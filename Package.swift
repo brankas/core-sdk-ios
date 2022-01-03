8829e2b8717ceb999fdee43f711ead3b02b740b9
Ejay Torres <ejay.torres@brank.as>
2022-01-03 12:01:30 +0800

    * Added minimum support for swift-tools-version 5.3 * Added minimum OS restriction for iOS 12 and above

--- Package.swift
+++ Package.swift
@@ -1,10 +1,14 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Tap Framework",
+    platforms: [
+        // Only add support for iOS 12 and up.
+        .iOS(.v12)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
